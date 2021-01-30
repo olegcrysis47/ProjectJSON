e8e8f98084a44228de71274052ac71a93fb1d055
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-30 17:29:46 +0300

    Добавлен класс Company с полями: company_name, address, stocks. Добавлен конструктор по умолчанию и геттеры/сеттеры.

---
+++ src/main/java/ru/aristovo/JSONPack/Company.java
@@ -0,0 +1,36 @@
+package ru.aristovo.JSONPack;
+
+import java.util.List;
+
+public class Company {
+
+    private String company_name;
+    private String address;
+    private List<Stocks> stocks;
+
+    public Company() {};
+
+    public String getCompany_name() {
+        return company_name;
+    }
+
+    public void setCompany_name(String company_name) {
+        this.company_name = company_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Stocks> getStocks() {
+        return stocks;
+    }
+
+    public void setStocks(List<Stocks> stocks) {
+        this.stocks = stocks;
+    }
+}
