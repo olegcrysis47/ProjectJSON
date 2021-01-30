d2434defa81b8c1fc6d5c67a44fe861617459c41
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-30 17:28:36 +0300

    Добавлен класс Stocks с одним полем owner. Добавлены геттеры и сеттеры.

---
+++ src/main/java/ru/aristovo/JSONPack/Stocks.java
@@ -0,0 +1,13 @@
+package ru.aristovo.JSONPack;
+
+public class Stocks {
+    private String owner;
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
