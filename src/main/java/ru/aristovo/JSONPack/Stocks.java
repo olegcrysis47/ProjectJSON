e08eca00ec80dc99e501b4b79fef3a279d8ee51e
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-31 23:18:16 +0300

    Добавлено поле дата в класс Stocks

--- src/main/java/ru/aristovo/JSONPack/Stocks.java
+++ src/main/java/ru/aristovo/JSONPack/Stocks.java
@@ -1,7 +1,25 @@
 package ru.aristovo.JSONPack;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Stocks {
+
+    @JsonProperty("expiration_date")
+    private LocalDate expirationDate;
     private String owner;
+
+    private static DateTimeFormatter defaultDateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public String getExpirationDate() {
+        return expirationDate.format(defaultDateFormat);
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = LocalDate.parse(expirationDate, defaultDateFormat);
+    }
 
     public String getOwner() {
         return owner;
