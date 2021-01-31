0bf46fbb920c8e7deec1e0db37441fc35fcfb65f
Oleg Pushkarev <tyler98765@yandex.ru>
2021-02-01 00:51:52 +0300

    Добавлен метод viewCompany - Вывести все имеющиеся компании в формате «Краткое название» – «Дата основания 17/01/98»;

--- src/main/java/ru/aristovo/application/streamWork/MethodsWorkStream.java
+++ src/main/java/ru/aristovo/application/streamWork/MethodsWorkStream.java
@@ -1,10 +1,29 @@
 package ru.aristovo.application.streamWork;
 
+import ru.aristovo.application.JSONpack.Company;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 public class MethodsWorkStream {
 
     /*
     - Вывести все имеющиеся компании в формате «Краткое название» – «Дата основания 17/01/98»;
+     */
 
+    public static void viewCompany(List<Company> list) {
+
+        DateTimeFormatter formatterNew = DateTimeFormatter.ofPattern("dd/MM/yy");
+        DateTimeFormatter formatterOld = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        list.stream().forEach((c) -> System.out.println("Название компании " +
+                c.getCompanyName() +
+                " - Дата основания " +
+                LocalDate.parse(c.getDateFoundation(), formatterOld).format(formatterNew)));
+
+    }
+
+    /*
     - Вывести все ценные бумаги (их код, дату истечения и полное название организации-владельца),
     которые просрочены на текущий день, а также посчитать суммарное число всех таких бумаг;
 
