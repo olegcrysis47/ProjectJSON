f371eb0ce8c04309d9c9f406140e062943b6b315
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-31 21:27:36 +0300

    Добавлено поле localdate

--- src/main/java/ru/aristovo/JSONPack/Company.java
+++ src/main/java/ru/aristovo/JSONPack/Company.java
@@ -1,14 +1,28 @@
 package ru.aristovo.JSONPack;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
+
 
 public class Company {
 
     private String company_name;
     private String address;
+    private LocalDate date_foundation;
     private List<Stocks> stocks;
 
     public Company() {};
+
+    public LocalDate getDate_foundation() {
+        return date_foundation;
+    }
+
+    public void setDate_foundation(LocalDate date_foundation) {
+        this.date_foundation = date_foundation;
+    }
 
     public String getCompany_name() {
         return company_name;
